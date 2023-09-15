c4b4ac6c6140b3675ee74d07369399b0431762ef
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-09-15 14:47:45 +0800

    chore: update

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -29,26 +29,24 @@ const input = document.querySelector('#input')!
 input.addEventListener('change', (e) => {
   console.log(e.target.files[0])
   const file = e.target.files[0];
-
+  const fileName = file.name
+  console.log(file)
   const chunkSize = 1024 * 1024; // 1MB
   const size = file.size
+  console.log(size)
   let start = 0;
-  let end = Math.min(start, size)
+  let end = Math.min(chunkSize, size)
   let count = 0
   while(start < size) {
     const chunk = file.slice(start, end)
+    console.log(chunk)
     const formdata = new FormData()
-    formdata.append('file', chunk)
+    formdata.append('chunk', chunk)
     formdata.append('hash', `${count}`)
-    formdata.append('filename', `文件-`)
-
-    const headers = new Headers();
-    headers.append('x-file-name', encodeURIComponent(`文件切片${count}`));
-
-    fetch('http://localhost:3334/upload2',{
+    formdata.append('fileName', fileName)
+    fetch('http://localhost:3334/api/upload',{
       method: 'post',
       body: formdata,
-      headers: headers
     })
     .then(response => response.json())
     .then(data => {
@@ -62,5 +60,29 @@ input.addEventListener('change', (e) => {
     end = Math.min(start + chunkSize, size)
     count++
   }
+
+  // merge
+  fetch('http://localhost:3334/api/merge', {
+    method: 'post',
+    body: JSON.stringify({merge: true, fileName: fileName}),
+    headers: {
+      'Content-Type': 'application/json' // 设置请求的内容类型为JSON
+    },
+  }).then(res => res.json()).then(data => {
+    console.log(data)
+  })
 })
+
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
+
+// 生成一个长度为 10 的随机字符串
 
--- packages/server/index.js
+++ packages/server/index.js
@@ -4,8 +4,10 @@ const cors = require('cors')
 const multer = require('multer')
 const config = require('dotenv').config()
 const fs = require('fs')
+const fsp = require('fs/promises');
 const app = express()
-const formidable = require('formidable');
+const formidable = require('formidable').default;
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 const directoryPath = './uploads'
@@ -19,7 +21,7 @@ if(!fs.existsSync(directoryPath)) {
 
 const uploadsPath = path.resolve(__dirname, directoryPath)
 
-//进行文件存贮
+// //进行文件存贮
 const storage = multer.diskStorage({
   destination: function(req, file, callback) {
     callback(null, uploadsPath)
@@ -34,6 +36,11 @@ const storage = multer.diskStorage({
 const upload = multer({storage})
 
 app.use(cors())
+// 解析请求体的body
+// 也可以使用bodyParser
+// app.use(bodyParser.json())
+// express内置了解析json的
+app.use(express.json())
 
 app.post('/upload', upload.single('file'), (req,res) => {
   const filename = req.headers['x-file-name']
@@ -42,26 +49,77 @@ app.post('/upload', upload.single('file'), (req,res) => {
   res.json({url: `http://localhost:${PORT}/uploads/${req.file.filename}`})
 })
 
+// app.use(express.urlencoded({ extended: true })); // 解析表单数据
 // 测试大文件上传
-app.post('/upload2', (req, res, next) => {
-  const form = formidable.formidable({});
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    console.log(fields)
-
-    // res.json({ fields, files });
-  });
-  //相应值
-  res.json({url: `http://localhost:${PORT}/uploads`})
-})
-
 
 //上传成功后访问这个路径访问静态资源,也就是刚才上传的文件
 app.use('/uploads', express.static(uploadsPath))
 
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h2>With <code>"express"</code> npm package</h2>
+    <form action="/api/upload" enctype="multipart/form-data" method="post">
+      <div>Text field title: <input type="text" name="title" /></div>
+      <div>File: <input type="file" name="someExpressFiles" multiple="multiple" /></div>
+      <input type="submit" value="Upload" />
+    </form>
+  `);
+});
+
+let isDown = false
+app.post('/api/upload', (req, res, next) => {
+  const form = formidable({});
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    } 
+    const chunks = [];
+
+    const [chunk] = files.chunk;
+    const [hash] = fields.hash;
+    const [fileName] = fields.fileName;
+    // 创建临时文件 用于保存chunk
+    const chunkDir = path.resolve(directoryPath, 'chunkDir_' + fileName);
+    if(!fs.existsSync(chunkDir)) {
+      console.log()
+       fs.mkdirSync(chunkDir);
+    }
+    // 将二进制写入的临时目录下
+    console.log(typeof chunk)
+    // 异步
+    // await fsp.writeFile(`${chunkDir}/${fileName}-${hash}`, await fsp.readFile(chunk.filepath))
+    // 同步
+    fs.writeFileSync(`${chunkDir}/${fileName}-${hash}`, fs.readFileSync(chunk.filepath))
+    res.json({ fields, files });
+  });
+});
+
+
+app.post('/api/merge', (req,res) => {
+  console.log(req.body,"???")
+  const fileName = req.body.fileName
+  // 获取目录
+  const filePath = path.resolve(directoryPath, 'chunkDir_' + fileName)
+  
+  mergeFileChunk(filePath, fileName);
+
+  // 读取指定目录, 然后进行合并
+  res.json({code: 0})
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
+
+
+function mergeFileChunk(filePath, fileName) {
+    console.log('fileath1:', filePath)
+    const files =  fs.readdirSync(filePath)
+    files.sort((a, b) => a.split("-").at(-1) - b.split("-").at(-1));
+    console.log("files:", files)
+    // 排序
+}
