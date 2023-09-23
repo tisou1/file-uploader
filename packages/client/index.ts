ffeb4932a245759871a3b91132947eca071406fb
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-09-23 12:01:47 +0800

    chore: async upload file done

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,13 +1,12 @@
 import { FileUploader, TASK_STATUS } from './fileUploader'
 import type { Task } from './fileUploader'
-const taskTemplate = document.querySelector('#template-task') as any;
+const taskTemplate = document.querySelector('#template-task') as any
 
 new FileUploader({
   element: document.querySelector('.dnd-file-uploader')!,
   uploadUrl: 'http://localhost:3334/upload',
-  taskRenderer: function(task: Task) {
-
-    const taskDOM = taskTemplate.content.firstElementChild.cloneNode(true);
+  taskRenderer: function (task: Task) {
+    const taskDOM = taskTemplate.content.firstElementChild.cloneNode(true)
     const nameDOM = taskDOM.querySelector('.task-name')
     nameDOM.textContent = task.name
     const progressDOM = taskDOM.querySelector('.task-progress')
@@ -16,73 +15,105 @@ new FileUploader({
     if (task.status === TASK_STATUS.PROCESSING) {
       taskDOM.style.background = `linear-gradient(to right, #bae7ff ${progress}, #fafafa ${progress}, #fafafa 100%)`
     } else if (task.status === TASK_STATUS.SUCCESS) {
-      taskDOM.style.background = '#d9f7be';
-      nameDOM.href = task.url;
+      taskDOM.style.background = '#d9f7be'
+      nameDOM.href = task.url
     } else if (task.status === TASK_STATUS.ERROR) {
-      taskDOM.style.background = '#ffccc7';
+      taskDOM.style.background = '#ffccc7'
     }
-    return taskDOM;
-  }
+    return taskDOM
+  },
 })
 
 const input = document.querySelector('#input')!
-input.addEventListener('change', (e) => {
+input.addEventListener('change', e => {
   console.log(e.target.files[0])
-  const file = e.target.files[0];
+  const file = e.target.files[0]
   const fileName = file.name
-  console.log(file)
-  const chunkSize = 1024 * 1024; // 1MB
-  const size = file.size
-  console.log(size)
-  let start = 0;
-  let end = Math.min(chunkSize, size)
-  let count = 0
-  while(start < size) {
-    const chunk = file.slice(start, end)
-    console.log(chunk)
-    const formdata = new FormData()
-    formdata.append('chunk', chunk)
-    formdata.append('hash', `${count}`)
-    formdata.append('fileName', fileName)
-    fetch('http://localhost:3334/api/upload',{
-      method: 'post',
-      body: formdata,
-    })
-    .then(response => response.json())
+
+  handleUpload(file)
+
+  // merge
+  fetch('http://localhost:3334/api/merge', {
+    method: 'post',
+    body: JSON.stringify({ merge: true, fileName: fileName }),
+    headers: {
+      'Content-Type': 'application/json', // 设置请求的内容类型为JSON
+    },
+  })
+    .then(res => res.json())
     .then(data => {
       console.log(data)
     })
-    .catch(e => {
-      console.log(e)
-    })  
+})
+
+async function handleUpload(file: File) {
+  // 进行分片
+  const fileChunkList = createFileChunk(file)
+  await uploadChunks(fileChunkList, file.name)
+}
+
+async function uploadChunks(fileChunkList, filename) {
+  const requestList = fileChunkList
+    .map(({ chunk, hash }) => {
+      const formdata = new FormData()
+      formdata.append('chunk', chunk)
+      formdata.append('hash', hash)
+      formdata.append('fileName', filename)
+
+      return { formdata }
+    })
+    .map(({ formdata }) => {
+      fetch('http://localhost:3334/api/upload', {
+        method: 'post',
+        body: formdata,
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    })
+
+  // 并发请求
+  await Promise.all(requestList)
+}
+
+function createFileChunk(file) {
+  const chunkSize = 1024 * 1024 // 1MB
+  const fileChunkList: {
+    chunk: File
+    hash: number
+  }[] = []
+  const size = file.size
+  let start = 0
+  let end = Math.min(chunkSize, size)
+  let count = 0
+  while (start < size) {
+    fileChunkList.push({
+      chunk: file.slice(start, end),
+      hash: count,
+    })
 
     start = end
     end = Math.min(start + chunkSize, size)
     count++
   }
 
-  // merge
-  fetch('http://localhost:3334/api/merge', {
-    method: 'post',
-    body: JSON.stringify({merge: true, fileName: fileName}),
-    headers: {
-      'Content-Type': 'application/json' // 设置请求的内容类型为JSON
-    },
-  }).then(res => res.json()).then(data => {
-    console.log(data)
-  })
-})
+  return fileChunkList
+}
 
 function generateRandomString(length) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  const charactersLength = characters.length;
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  const charactersLength = characters.length
   for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charactersLength);
-    result += characters.charAt(randomIndex);
+    const randomIndex = Math.floor(Math.random() * charactersLength)
+    result += characters.charAt(randomIndex)
   }
-  return result;
+  return result
 }
 
 // 生成一个长度为 10 的随机字符串
-
--- packages/server/index.js
+++ packages/server/index.js
@@ -4,16 +4,16 @@ const cors = require('cors')
 const multer = require('multer')
 const config = require('dotenv').config()
 const fs = require('fs')
-const fsp = require('fs/promises');
+const fsp = require('fs/promises')
 const app = express()
-const formidable = require('formidable').default;
+const formidable = require('formidable').default
 const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 const directoryPath = './uploads'
 
 // 如果uploads目录不存在,就创建
-if(!fs.existsSync(directoryPath)) {
+if (!fs.existsSync(directoryPath)) {
   fs.mkdirSync(directoryPath)
 } else {
   console.log('目录已存在')
@@ -23,17 +23,17 @@ const uploadsPath = path.resolve(__dirname, directoryPath)
 
 // //进行文件存贮
 const storage = multer.diskStorage({
-  destination: function(req, file, callback) {
+  destination: function (req, file, callback) {
     callback(null, uploadsPath)
   },
-  filename: function(req, file, callback) {
+  filename: function (req, file, callback) {
     const filename = req.headers['x-file-name']
     callback(null, `${Date.now()}-${decodeURIComponent(filename)}`)
     return
-  }
+  },
 })
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
 app.use(cors())
 // 解析请求体的body
@@ -42,11 +42,11 @@ app.use(cors())
 // express内置了解析json的
 app.use(express.json())
 
-app.post('/upload', upload.single('file'), (req,res) => {
+app.post('/upload', upload.single('file'), (req, res) => {
   const filename = req.headers['x-file-name']
   console.log('req', req.file, filename)
   //相应值
-  res.json({url: `http://localhost:${PORT}/uploads/${req.file.filename}`})
+  res.json({ url: `http://localhost:${PORT}/uploads/${req.file.filename}` })
 })
 
 // app.use(express.urlencoded({ extended: true })); // 解析表单数据
@@ -54,7 +54,6 @@ app.post('/upload', upload.single('file'), (req,res) => {
 
 //上传成功后访问这个路径访问静态资源,也就是刚才上传的文件
 app.use('/uploads', express.static(uploadsPath))
-
 
 app.get('/', (req, res) => {
   res.send(`
@@ -64,87 +63,150 @@ app.get('/', (req, res) => {
       <div>File: <input type="file" name="someExpressFiles" multiple="multiple" /></div>
       <input type="submit" value="Upload" />
     </form>
-  `);
-});
+  `)
+})
 
 let isDown = false
 app.post('/api/upload', (req, res, next) => {
-  const form = formidable({});
+  const form = formidable({})
 
-  form.parse(req, (err, fields, files) => {
+  form.parse(req, async (err, fields, files) => {
     if (err) {
-      next(err);
-      return;
-    } 
-    const chunks = [];
-
-    const [chunk] = files.chunk;
-    const [hash] = fields.hash;
-    const [fileName] = fields.fileName;
-    // 创建临时文件 用于保存chunk
-    const chunkDir = path.resolve(directoryPath, 'chunkDir_' + fileName);
-    if(!fs.existsSync(chunkDir)) {
-       fs.mkdirSync(chunkDir);
+      next(err)
+      return
     }
-    // 将二进制写入的临时目录下
-    // 异步
-    // await fsp.writeFile(`${chunkDir}/${fileName}-${hash}`, await fsp.readFile(chunk.filepath))
-    // 同步
-    fs.writeFileSync(`${chunkDir}/${fileName}-${hash}`, fs.readFileSync(chunk.filepath))
-    res.json({ fields, files });
-  });
-});
+    // 同步保存chunk
+    // saveFileChunkSync(fields)
 
+    // 一步保存chunk
+    // console.log(files, files)
+    await saveFileChunkAsync(files, fields)
 
-app.post('/api/merge', (req,res) => {
-  console.log(req.body,"???")
-  const fileName = req.body.fileName
-  // 获取目录
-  // const filePath = path.resolve(directoryPath, 'chunkDir_' + fileName)
-  
-  // mergeFileChunk(filePath, fileName);
-
-  // 读取指定目录, 然后进行合并
-  res.json({code: 0})
+    res.json({ fields, files })
+  })
 })
 
+app.post('/api/merge', async (req, res) => {
+  console.log(req.body, '???')
+  const fileName = req.body.fileName
+  // 获取目录
+  const filePath = path.resolve(directoryPath, 'chunkDir_' + fileName)
+
+  await mergeFileChunkAsync(filePath, fileName)
+
+  // 读取指定目录, 然后进行合并
+  res.json({ code: 0 })
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
 
-
 function mergeFileChunk(filePath, fileName) {
   // todo
-    console.log('fileath1:', filePath)
-    setTimeout(() => {
-      const files =  fs.readdirSync(filePath)
-      files.sort((a, b) => a.split("-").at(-1) - b.split("-").at(-1));
-      console.log("files:", uploadsPath + '/' + fileName)
+  console.log('fileath1:', filePath)
+  setTimeout(() => {
+    const files = fs.readdirSync(filePath)
+    console.log('files:', files)
 
-      files.forEach((chunkpath) => {
-        // console.log(path.resolve(filePath, chunkpath),'>>>>>>>>>')
-        pipeStream(
-        path.resolve(filePath, chunkpath),
-        // 根据 size 在指定位置创建可写流
-          fs.createWriteStream(uploadsPath + '/' + fileName)
-        )
-      })
-    })
+    return
+    files.sort((a, b) => a.split('-').at(-1) - b.split('-').at(-1))
+    const chunkSize = 1024 * 1024 // 1MB
+    // files.forEach((chunkpath, index) => {
+    //   pipeStream(
+    //     path.resolve(filePath, chunkpath),
+    //     // 根据 size 在指定位置创建可写流
+    //     fs.createWriteStream(uploadsPath + '/' + fileName, {
+    //       start: index * chunkSize,
+    //     })
+    //   )
+    // })
+  })
 }
 
-mergeFileChunk('/Users/v_wangtao34/t/file-uploader/packages/server/uploads/chunkDir_4.11 作业建模重构-训练作业 1206.pdf','4.11 作业建模重构-训练作业 1206.pdf')
+// mergeFileChunk(
+//   'D:/t/file-uploader/packages/server/uploads/chunkDir_jQuery权威指南',
+//   'jQuery权威指南.pdf'
+// )
 
 function pipeStream(path, writeStream) {
   const readStream = fs.createReadStream(path)
   readStream.on('end', () => {
     // 删除已合并的切片文件（可选）
-    // fs.unlink(path, err=> {
-    //   if (err) {
-    //     console.error('删除切片文件失败:', err);
-    //   }
-    // })
+    fs.unlink(path, err => {
+      if (err) {
+        console.error('删除切片文件失败:', err)
+      }
+    })
   })
 
   readStream.pipe(writeStream)
+}
+
+function saveFileChunkSync(files, fields) {
+  const [chunk] = files.chunk
+  const [hash] = fields.hash
+  const [fileName] = fields.fileName
+  // 创建临时文件 用于保存chunk
+  const chunkDir = path.resolve(directoryPath, 'chunkDir_' + fileName)
+  if (!fs.existsSync(chunkDir)) {
+    fs.mkdirSync(chunkDir)
+  }
+  // 将二进制写入的临时目录下
+  // 异步
+  // await fsp.writeFile(`${chunkDir}/${fileName}-${hash}`, await fsp.readFile(chunk.filepath))
+  // 同步
+  fs.writeFileSync(
+    `${chunkDir}/${fileName}-${hash}`,
+    fs.readFileSync(chunk.filepath)
+  )
+}
+
+async function mergeFileChunkAsync(dirPath, fileName) {
+  const files = await fsp.readdir(dirPath)
+  console.log('files:', files, dirPath, fileName)
+  files.sort((a, b) => a.split('-').at(-1) - b.split('-').at(-1))
+  const chunkSize = 1024 * 1024 // 1MB
+  for (const file of files) {
+    const filePath = path.join(dirPath, file)
+    const data = await fsp.readFile(filePath)
+    await fsp.appendFile(uploadsPath + '/' + fileName, data)
+    await fsp.unlink(filePath) // 删除已合并的切片
+  }
+  // files.forEach((chunkpath, index) => {
+  //   pipeStream(
+  //     path.resolve(filePath, chunkpath),
+  //     // 根据 size 在指定位置创建可写流
+  //     fsp.createWriteStream(uploadsPath + '/' + fileName, {
+  //       start: index * chunkSize,
+  //     })
+  //   )
+  // })
+}
+
+async function saveFileChunkAsync(files, fields) {
+  const [chunk] = files.chunk
+  const [hash] = fields.hash
+  const [fileName] = fields.fileName
+  // 创建临时文件 用于保存chunk
+  const chunkDir = path.resolve(directoryPath, 'chunkDir_' + fileName)
+  if (!(await directoryExists(chunkDir))) {
+    const createDir = await fsp.mkdir(chunkDir)
+    console.log('dir:', createDir)
+  }
+
+  // 异步保存
+  await fsp.writeFile(
+    `${chunkDir}/${fileName}-${hash}`,
+    await fsp.readFile(chunk.filepath)
+  )
+}
+
+async function directoryExists(path) {
+  try {
+    await fsp.access(path, fs.constants.F_OK)
+    return true // 目录存在
+  } catch (err) {
+    return false // 目录不存在
+  }
 }
